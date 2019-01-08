1403ad74136805b76e64a7f55e60890f742a156b
tanaya tanaya <tanayagaind@gmail.com>
2019-01-08 15:47:28 -0500

    Add files via upload

---
+++ build/lk/CMakeFiles/lk.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/lk.dir/lk.c.o"
+  "lk.pdb"
+  "lk"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/lk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/prod-cons/CMakeFiles/t-unsafe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/t-unsafe.dir/t-unsafe.c.o"
+  "t-unsafe.pdb"
+  "t-unsafe"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/t-unsafe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
